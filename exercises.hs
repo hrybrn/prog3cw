d8cbf0b4da21482f9face85c94c6d9c359764dac
Unknown <harryjgbrown@gmail.com>
2017-10-22 18:54:00 +0100

    finished ex1, started ex3

--- exercises.hs
+++ exercises.hs
@@ -1,3 +1,16 @@
 --exercise 1
 subtotal :: Num a => [a] -> [a]
-subtotal c (x:xs) = subtotal
+subtotal (x:[]) = [x]
+subtotal (x:y:xs) = [x] ++ subtotal ([z] ++ xs)
+    where
+        z = x + y
+
+--exercise 2
+--histogram :: Int -> [Int] -> [Int]
+
+--exercise 3
+meetsOffer :: String -> Num -> Bool
+meetsOffer (x:xs) y =
+
+gradeToPoints :: Char -> Num
+gradeToPoints x = 
